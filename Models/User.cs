f1a970a7e5702d087b6eb197e7379aeadff70e80
IGORMASL <workigormas.20@gmail.com>
2025-11-11 19:21:19 +0700

    Add user model

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GanttChartAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+       
+        [Required]
+        public string PasswordHash { get; set; }
+    }
+}
